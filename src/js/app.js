695fee2884964d9c8aa259d17dfbdb9c1ac5a0fe
James Rock <hello@jamesrock.me>
2026-03-05 13:44:06 +0000

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,8 @@ import {
   getDefaultTime
 } from './utils';
 import {
-  Rounder,
   setDocumentHeight,
+  addDragListeners,
   makeNode,
   makeButton,
   makeToggle,
@@ -27,7 +27,6 @@ const sqareSize = makeEven(limit(Math.round(window.innerWidth / 10), 50));
 const puzzleSize = sqareSize*9;
 const solvedNode = makeNode('div', 'solved');
 const eventsNode = makeNode('div', 'events');
-const rounder = new Rounder(40);
 const rotateKeys = ['Space'];
 const directionKeysMap = {
   'ArrowUp': 'up',
@@ -40,14 +39,11 @@ const board = makeNode('div', 'board');
 let savedGame = storage.get('saved');
 let times = storage.get('times');
 let start = 0;
-let touch = null;
-let xMovement = 0;
-let yMovement = 0;
-let movement = 0;
 let puzzle = null;
 let renderer = null;
 let mode = 'standard';
 let difficulty = 'easy';
+let moved = false;
 
 if(savedGame) {
   mode = savedGame[2];
@@ -142,59 +138,35 @@ const init = () => {
 
 };
 
-eventsNode.addEventListener('touchstart', (e) => {
-
-  touch = e.touches[0];
-  movement = 0;
-  xMovement = 0;
-  yMovement = 0;
-
-  e.preventDefault();
-
-}, {passive: false});
-
-eventsNode.addEventListener('touchmove', (e) => {
-
-  const {clientX: originalClientX, clientY: originalClientY} = touch;
-  const {clientX, clientY} = e.touches[0];
-  const x = rounder.round(clientX - originalClientX);
-  const y = rounder.round(clientY - originalClientY);
-
-  if(x !== xMovement) {
-    eventsNode.dispatchEvent(new Event(x > xMovement ? 'drag-right' : 'drag-left'));
-  };
-
-  if(y !== yMovement) {
-    eventsNode.dispatchEvent(new Event(y > yMovement ? 'drag-down' : 'drag-up'));
-  };
-
-  xMovement = x;
-  yMovement = y;
-  movement += (xMovement + yMovement);
-
-});
+addDragListeners(eventsNode, 40);
 
 eventsNode.addEventListener('touchend', () => {
 
-  if(movement === 0) {
+  if(!moved) {
     puzzle ? puzzle.fill() : startNewGame();
   };
+
+  moved = false;
 
 });
 
 eventsNode.addEventListener('drag-down', () => {
+  moved = true;
   puzzle.move('down');
 });
 
 eventsNode.addEventListener('drag-up', () => {
+  moved = true;
   puzzle.move('up');
 });
 
 eventsNode.addEventListener('drag-right', () => {
+  moved = true;
   puzzle.move('right');
 });
 
 eventsNode.addEventListener('drag-left', () => {
+  moved = true;
   puzzle.move('left');
 });
 
